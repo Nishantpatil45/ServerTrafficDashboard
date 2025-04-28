a2a82cf47f4f24f0ac4c51018444825b0fbcec70
Nishant Patil <nishanthp@weblineapps.com>
2025-04-28 11:57:53 +0530

    - Added new compoenents for every section like tickets, new - server. - Added common used compoenets like buttons, control and used common code accross the board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,14 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [RouterOutlet, HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'servertrafficdashbpard';
-
-  
-
 }
